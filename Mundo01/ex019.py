267f2da7698e7b3a91bbe9ad11a8bf9ce55b679e
Victor Barreto <denvictor.sbarreto@gmail.com>
2023-01-16 19:58:17 -0300

    Sorteando um item na lista

---
+++ Mundo01/ex019.py
@@ -0,0 +1,15 @@
+import random
+
+lista = []
+
+n1 = str(input("Primeiro aluno: "))
+n2 = str(input("Segundo aluno: "))
+n3 = str(input("Terceiro aluno: "))
+n4 = str(input("Quarto aluno: "))
+
+lista.append(n1)
+lista.append(n2)
+lista.append(n3)
+lista.append(n4)
+
+print(f"O aluno escolhido foi {random.choice(lista)}")
